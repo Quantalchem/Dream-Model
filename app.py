71706860013d79fab782115b590d944f4d64d407
Quantalchem <74022063+Quantalchem@users.noreply.github.com>
2023-06-15 06:52:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+import os
+from project import main
 
-@app.route("/")
-def index():
-    return "<p>Hello, World!</p>"
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    main.app.run(host='0.0.0.0', port=port)
